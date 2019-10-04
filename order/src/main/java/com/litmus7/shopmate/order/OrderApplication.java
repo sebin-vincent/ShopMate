d3769e0f28c2bc2cd445b64e07b9b2004bcd9dd3
shihab <shihabvk99@gmail.com>
2019-10-04 09:55:06 +0530

    order project added

---
+++ order/src/main/java/com/litmus7/shopmate/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.litmus7.shopmate.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
---
+++ order/src/test/java/com/litmus7/shopmate/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litmus7.shopmate.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
