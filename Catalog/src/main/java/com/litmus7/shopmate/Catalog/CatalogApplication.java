f5112c7acaa356f57d40d9dc9b6c9ba44c48d54c
Job <job.daniel@litmus7.com>
2019-10-09 18:06:43 +0530

    Catalog

--- Catalog/src/main/java/com/litmus7/shopmate/Catalog/CatalogApplication.java
+++ Catalog/src/main/java/com/litmus7/shopmate/Catalog/CatalogApplication.java
@@ -2,8 +2,10 @@ package com.litmus7.shopmate.Catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = {"com.litmus7.shopmate"}) 
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.litmus7.shopmate"})
 public class CatalogApplication {
 
 	public static void main(String[] args) {
--- Catalog/src/main/java/com/litmus7/shopmate/Services/CatalogService.java
+++ Catalog/src/main/java/com/litmus7/shopmate/Services/CatalogService.java
@@ -3,22 +3,22 @@ package com.litmus7.shopmate.Services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
 
 import com.litmus7.shopmate.Dto.Catalog;
 import com.litmus7.shopmate.Repository.CatalogRepository;
 
-
+@ComponentScan("com.litmus7.shopmate.Repository")
 @Service
-
 public class CatalogService {
 
 	@Autowired
 	private CatalogRepository catalogRepository;
 	
 	public Optional<Catalog> getMenu() {
-		Optional<Catalog> menu =  catalogRepository.findById(1L);
 		
+		Optional<Catalog> menu =  catalogRepository.findById(1L);
 		return menu;
 	}
 	
