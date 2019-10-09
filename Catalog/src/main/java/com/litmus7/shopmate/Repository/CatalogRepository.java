85c11008df92f14ad7558d5da1bf24d6c2e80cb4
sebin.vincent <sebin.vincent@litmus7.com>
2019-10-09 18:20:57 +0530

    catalog corrected

--- Catalog/src/main/java/com/litmus7/shopmate/Repository/CatalogRepository.java
+++ Catalog/src/main/java/com/litmus7/shopmate/Repository/CatalogRepository.java
@@ -1,10 +1,12 @@
 package com.litmus7.shopmate.Repository;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.litmus7.shopmate.Dto.Catalog;
 
+@EntityScan("com.litmus7.shopmate.Dto")
 @Repository
 public interface CatalogRepository extends CrudRepository<Catalog, Long> {
 
