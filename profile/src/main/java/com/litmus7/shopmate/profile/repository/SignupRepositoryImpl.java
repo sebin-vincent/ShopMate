40d4ac9aea66fa3b7409d9e806893be291edd79f
resmi <resmi.puthirippadath@litmus7.com>
2019-10-16 11:01:23 +0530

    profile sql and profile

---
+++ profile/src/main/java/com/litmus7/shopmate/profile/repository/SignupRepositoryImpl.java
@@ -0,0 +1,71 @@
+package com.litmus7.shopmate.profile.repository;
+
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.stereotype.Repository;
+
+import com.litmus7.shopmate.profile.dto.Response_Info;
+import com.litmus7.shopmate.profile.dto.UserDto;
+import com.litmus7.shopmate.profile.exception.UserExistException;
+import com.litmus7.shopmate.profile.repositorydao.SignupRepositoryDao;
+
+
+@EntityScan("com.litmus7.shopmate.profile.dto")
+@Repository
+public class SignupRepositoryImpl implements SignupRepositoryDao {
+
+	@PersistenceContext
+	EntityManager entityManager;
+	Response_Info responseInfo =new Response_Info();
+	@SuppressWarnings("unchecked")
+	@Transactional
+	@Override
+	public Response_Info saveNewUser(UserDto userObject) {
+		// TODO Auto-generated method stub
+
+		boolean isUserExists = isUserAlreadyExist(userObject);
+		if (isUserExists == true) {
+			entityManager.merge(userObject);
+			responseInfo.setStatus_Code(200);
+			responseInfo.setStatus_Message("User added successfully");
+			responseInfo.setPayload(null);
+			return responseInfo;
+		} else {
+			responseInfo.setStatus_Code(400);
+			responseInfo.setStatus_Message("User already exist");
+			responseInfo.setPayload(null);
+			return responseInfo;
+		}
+
+	}
+
+	public boolean isUserAlreadyExist(UserDto userObject) {
+		// TODO Auto-generated method stub
+		String email = userObject.getEmail();
+		Query query = entityManager.createQuery("select profileId from UserDto where email=?1");
+		query.setParameter(1, email);
+		@SuppressWarnings("unchecked")
+		List<Integer> resultList = query.getResultList();
+		if (!resultList.isEmpty()) {
+			throw new UserExistException("User already exist");
+		}
+
+		// entityManager.createQuery("select profileId from UserDto where
+		// email=?1").setParameter(1, email).executeUpdate();
+		else {
+			System.out.println(email);
+
+			return true;
+		}
+
+	}
+
+}
+
---
+++ profile/src/main/java/com/litmus7/shopmate/profile/repositorydao/SignupRepositoryDao.java
@@ -0,0 +1,15 @@
+package com.litmus7.shopmate.profile.repositorydao;
+
+import org.springframework.stereotype.Repository;
+
+import com.litmus7.shopmate.profile.dto.Response_Info;
+import com.litmus7.shopmate.profile.dto.UserDto;
+
+@Repository
+public interface SignupRepositoryDao{
+	
+	public Response_Info saveNewUser(UserDto userObject);
+	//@Query("select * from User where email=1?")
+	//UserDto findByUserDto (String email);
+	
+}
