44d8bd9eefd248ab4595b12052ab432b0a221bb6
Amjad Sibili K T <amjad.sibili@litmus7.com>
2019-10-17 15:47:38 +0530

    integrated trending ui

--- order/src/main/java/com/litmus7/shopmate/order/controller/TrendingController.java
+++ order/src/main/java/com/litmus7/shopmate/order/controller/TrendingController.java
@@ -3,10 +3,7 @@ package com.litmus7.shopmate.order.controller;
 import com.litmus7.shopmate.order.dto.Response;
 import com.litmus7.shopmate.order.service.TrendingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
@@ -15,6 +12,7 @@ public class TrendingController {
     @Autowired
     private TrendingServiceImpl trendingService;
 
+    @CrossOrigin
     @GetMapping("trending/{limit}")
     public Response getTrending(@PathVariable int limit) {
 
