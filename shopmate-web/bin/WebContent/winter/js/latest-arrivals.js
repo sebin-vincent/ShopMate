8188f28f77b83bce24645c68cb02bb950b0f37ba
neenu.joshy <neenu.joshy@litmus7.com>
2019-10-23 13:21:23 +0530

    conflicts resolved

---
+++ shopmate-web/bin/WebContent/winter/js/latest-arrivals.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8083/items/latest-arrivals/6",
+    
+        success: function (responseFromInventory) {
+
+            responseFromInventory.payload.forEach(element => {
+                
+                $.ajax({
+                    type: "GET",
+                    url: "http://localhost:8082/sku/latest/" + element.skuId,
+                   
+                    success: function (responseFromCatalog) {
+
+                        var baseTemplate = `
+
+                        <div class="latest-arrived-item">
+                        <a href="${element.skuPDPUrl}"> <img src="${responseFromCatalog.skuImage}" alt="${element.skuName}"> </a>
+                        <div class="latest-arrived-item-text">${element.skuName}</div>
+                        </div>
+
+                        `;
+
+                        $("#latest-arrivals-cont").append(baseTemplate);
+                        
+                    }
+                });
+
+            });
+            
+        }
+    });
+
+});
