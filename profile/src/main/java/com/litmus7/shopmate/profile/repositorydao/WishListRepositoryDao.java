1e691aebf9c1eeeaa672acb97c73c4e621c46cd8
resmi <resmi.puthirippadath@litmus7.com>
2019-10-23 12:28:15 +0530

    plp and pdp integration

--- profile/src/main/java/com/litmus7/shopmate/profile/repositorydao/WishListRepositoryDao.java
+++ profile/src/main/java/com/litmus7/shopmate/profile/repositorydao/WishListRepositoryDao.java
@@ -1,13 +1,16 @@
 package com.litmus7.shopmate.profile.repositorydao;
 
+import com.litmus7.shopmate.profile.dto.Response_Info;
 import com.litmus7.shopmate.profile.dto.WishListDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface WishListRepositoryDao extends JpaRepository<WishListDto, Integer> {
 
-//    public WishListDto addWishlist(WishListDto wishlist);
+	public WishListDto findBySkuIdAndProfileId(int skuId,int profileId);
     public List<WishListDto> findByProfileId(int profileId);
+    
 
 }
--- profile/src/main/java/com/litmus7/shopmate/profile/service/WishListService.java
+++ profile/src/main/java/com/litmus7/shopmate/profile/service/WishListService.java
@@ -1,6 +1,7 @@
 package com.litmus7.shopmate.profile.service;
 
 import com.litmus7.shopmate.profile.dao.WishListServiceDao;
+import com.litmus7.shopmate.profile.dto.Response_Info;
 import com.litmus7.shopmate.profile.dto.WishListDto;
 //import com.litmus7.shopmate.profile.repository.WishListRepository;
 import com.litmus7.shopmate.profile.repositorydao.WishListRepositoryDao;
@@ -12,26 +13,46 @@ import java.util.List;
 
 @Service
 public class WishListService implements WishListServiceDao {
+	boolean isIdexist = true;
+	@Autowired
+	private WishListRepositoryDao wishListRepositoryDao;
+	Response_Info response= new Response_Info();
 
-    @Autowired
-    private WishListRepositoryDao wishListRepositoryDao;
+	@Override
+	public Response_Info addToUserWishList(WishListDto wishListDto) {
 
-    @Override
-    public WishListDto addToUserWishList(WishListDto wishListDto) {
+		WishListDto wish = wishListRepositoryDao.findBySkuIdAndProfileId(wishListDto.getSkuId(),
+				wishListDto.getProfileId());
+		System.out.println(wish);
+		if(wish==null) {
+			wishListRepositoryDao.save(wishListDto);
+			response.setStatus_Code(200);
+			response.setPayload(null);
+			response.setStatus_Message("item added");
+			return response;
+			
+		}
+		else {
+			response.setStatus_Code(200);
+			response.setPayload(null);
+			response.setStatus_Message("item exist");
+			return response;
+		}
+		
 
-        return wishListRepositoryDao.save(wishListDto);
+		
 
-    }
+	}
 
-    @Override
-    public List<Integer> fetchWishListByUserId(int profileId) {
+	@Override
+	public List<Integer> fetchWishListByUserId(int profileId) {
 
-        List<Integer> skuIds = new ArrayList<>();
-        for (WishListDto wishlistItem : wishListRepositoryDao.findByProfileId(profileId)) {
-            skuIds.add(wishlistItem.getSkuId());
-        }
+		List<Integer> skuIds = new ArrayList<>();
+		for (WishListDto wishlistItem : wishListRepositoryDao.findByProfileId(profileId)) {
+			skuIds.add(wishlistItem.getSkuId());
+		}
 
-        return skuIds;
+		return skuIds;
 
-    }
+	}
 }
