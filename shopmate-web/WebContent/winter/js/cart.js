3f584d91316e8f9df4496daa0d408a2bcde5efd3
Job <job.daniel@litmus7.com>
2019-10-31 09:56:08 +0530

    remove

--- shopmate-web/WebContent/winter/js/cart.js
+++ shopmate-web/WebContent/winter/js/cart.js
@@ -120,93 +120,7 @@ $(document).ready(function () {
 
                     }
 
-                    function quantityChanged(event) {
-                        var input = event.target
-                        if (isNaN(input.value) || input.value <= 0) {
-                            input.value = 1
-                        }
-                        
-                        var id =$(input.parentElement.parentElement.parentElement.parentElement).attr('id')
-                       
-                        updateInventoryData = {
-
-                            "skuId": id,
-                            "quantity": input.value
-      
-                          }
-      
-                          $.ajax({
-      
-                            type: "PUT",
-                            url: "http://localhost:8083/items/reserve",
-                            data: JSON.stringify(updateInventoryData),
-                            dataType: "json",
-                            contentType: "application/json; charset=utf-8",
-                            async:false,
-      
-                            success: function (responseFromInventory) {
-      
-                              alert("quantity updated")
-      
-                            }
-                          });
-
-                        subTotal()
-
-                    }
-
-                    function removeItem(event) {
-                        var buttonClicked = event.target
-                        // console.log(event.data)
-
-                        buttonClicked.parentElement.parentElement.parentElement.parentElement.remove()
-                        var id = $(buttonClicked.parentElement.parentElement.parentElement.parentElement).attr('id')
-
-
-                        var removeUrl = `http://localhost:8081/cart/delete/?orderId=${orderId}&skuId=${id}`
-
-                        $.ajax({
-                            type: "delete",
-                            url: removeUrl,
-                            async: false,
-                            success: function (response) {
-
-                            }
-                        });
-
-
-
-
-                        subTotal()
-                    }
-
-                    function subTotal() {
-
-                        var cartItems = document.getElementById("cart-item")
-
-                        var cartRows = cartItems.getElementsByClassName("cart-row")
-
-                        var subtotal = 0
-                        for (let k = 0; k < cartRows.length; k++) {
-                            var cartRow = cartRows[k]
-                            var priceElement = cartRow.getElementsByClassName("item-price")[0]
-
-                            var quantityElement = cartRow.getElementsByClassName("cart-quantity-input")[0]
-
-                            var price = parseFloat(priceElement.innerText)
-                            var quantity = quantityElement.value
-                            sessionStorage.setItem("quantity",quantity);
-                            var total = price * quantity
-                            document.getElementsByClassName("total")[k].innerHTML = `<h5> ${total}</h5>`
-
-
-                            subtotal = subtotal + (price * quantity)
-                            sessionStorage.setItem("subtotal",subtotal);
-                          
-                        }
-
-                        document.getElementById("subtotal").innerHTML = `${subtotal}`
-                    }
+                  
 
                 }
 
@@ -227,4 +141,133 @@ function logout() {
    console.log(sessionStorage.getItem("profile_id"))
   
    }
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    
+    var id =$(input.parentElement.parentElement.parentElement.parentElement).attr('id')
+   
+   
+
+    subTotal()
+
+    }
+
+function removeItem(event) {
+    var buttonClicked = event.target
+    // console.log(event.data)
+
+    buttonClicked.parentElement.parentElement.parentElement.parentElement.remove()
+    var id = $(buttonClicked.parentElement.parentElement.parentElement.parentElement).attr('id')
+
+
+    var removeUrl = `http://localhost:8081/cart/delete/?orderId=${orderId}&skuId=${id}`
+
+    $.ajax({
+        type: "delete",
+        url: removeUrl,
+        async: false,
+        success: function (response) {
+
+        }
+    });
+    var quantity = input.value
+    if(input.value==1){
+        quantity = 0;
+    }
+  
+    updateInventoryData = {
+
+        "skuId": id,
+        "quantity": quantity
+
+      }
+
+      $.ajax({
+
+        type: "PUT",
+        url: "http://localhost:8083/items/restore/",
+        data: JSON.stringify(updateInventoryData),
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        async:false,
+
+        success: function (responseFromInventory) {
+
+         // alert("quantity updated")
+
+        }
+      });
+
+
+
+
+
+    subTotal()
+    }
+
+function subTotal() {
+
+    var cartItems = document.getElementById("cart-item")
+
+    var cartRows = cartItems.getElementsByClassName("cart-row")
+
+    var subtotal = 0
+    for (let k = 0; k < cartRows.length; k++) {
+        var cartRow = cartRows[k]
+        var priceElement = cartRow.getElementsByClassName("item-price")[0]
+
+        var quantityElement = cartRow.getElementsByClassName("cart-quantity-input")[0]
+
+        var price = parseFloat(priceElement.innerText)
+        var quantity = quantityElement.value
+        sessionStorage.setItem("quantity",quantity);
+        var total = price * quantity
+        document.getElementsByClassName("total")[k].innerHTML = `<h5> ${total}</h5>`
+
+
+        subtotal = subtotal + (price * quantity)
+        sessionStorage.setItem("subtotal",subtotal);
+      
+    }
+
+    document.getElementById("subtotal").innerHTML = `${subtotal}`
+    }
+
+
+function checkout(){
+    var cartRows = document.getElementsByClassName("cart-row")
+    
+    for (let index = 0; index < cartRows.length; index++) {
+        var skuId = $(cartRows[index]).attr('id')
+        var quantity= document.getElementsByClassName("cart-quantity-input")[index].value
+
+        updateInventoryData = {
+
+            "skuId": skuId,
+            "quantity": quantity
+    
+          }
+    
+          $.ajax({
+    
+            type: "PUT",
+            url: "http://localhost:8083/items/restore/",
+            data: JSON.stringify(updateInventoryData),
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            async:false,
+    
+            success: function (responseFromInventory) {
+    
+             
+    
+            }
+          });
+        
+    }
+}
+
 
